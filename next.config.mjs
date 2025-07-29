ff83c89f95f90d7de42bae0734fe1ba2d941f942
alanchrissantony <alanchrissantony@gmail.com>
2025-07-29 11:58:15 +0530

    (fix): updated protocols

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8080',
+        protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL,
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME,
+        port: process.env.NEXT_PUBLIC_IMAGE_PORT,
       },
     ],
   },
